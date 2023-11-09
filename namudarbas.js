97df0bfb6bf17e94007aba8704ae7683fb488e5f
zegnex <baxyranbaxy@gmail.com>
2023-11-09 19:43:17 +0200

    first commit

---
+++ namudarbas.js
@@ -0,0 +1,18 @@
+let vardas = "John"
+let pavarde = "Doe"
+let skliaustas = "("
+let skliaustas2 = ")"
+let amzius = "amzius"
+let metai = 25
+let m = "m"
+let taskas = "."
+let kablelis = ","
+let asmensKodas = "asmens kodas"
+let dvitaskis = ":"
+let ak = 37878787878
+let gyvVieta = "gyvenamasis miestas"
+let miestas = "Kaunas"
+let tarpas = " "
+
+
+console.log(vardas+tarpas+pavarde+tarpas+skliaustas+amzius+tarpas+metai+tarpas+m+taskas+skliaustas2+kablelis+tarpas+asmensKodas+dvitaskis+tarpas+ak+kablelis+tarpas+gyvVieta+dvitaskis+tarpas+miestas+taskas)
---
+++ scripto_paaiskinimai.js
@@ -0,0 +1,23 @@
+// kintamojo sukurimas:
+// 1. Inicijavimo zodelis (var, let, const)
+// 2. Kintamojo pavadinimas
+// 3. Lygybes zenklas =
+// 4. Kintamojo verte (Value)
+
+
+var vardas = "John"  // String (tekstas) duomenu tipas
+let pavarde = "Doe" // String (tekstas) duomenu tipas
+let miestas = "Kaunas" // String (tekstas) duomenu tipas
+let amzius = 30 // Number (skaicius) duomenu tipas
+let YraStudentas = true // Boolean (true/false) duomenu tipas
+const asmenskodas = 37878787878 // Number (skaicius) duomenu tipas
+
+console.log(vardas)
+console.log(pavarde)
+console.log(miestas)
+console.log(amzius)
+console.log(YraStudentas)
+
+
+
+
